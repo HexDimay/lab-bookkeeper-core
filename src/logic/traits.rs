72c9f8587ac194113219c78b63c0e1255b8f7804
0xDimay <HexDimay@yandex.ru>
2026-05-19 20:08:11 +0300

    Add new functions.

--- src/logic/traits.rs
+++ src/logic/traits.rs
@@ -8,12 +8,19 @@ use crate::components::{
 
 /// `HandleData` - слушатель данных, что позволяет `Commander` осуществлять все требуемые комманды.
 pub trait HandleData<A: Arg> {
-    fn get_entitys(&self) -> &[Entity<A>];
-    fn get_mut_entitys(&mut self) -> &mut [Entity<A>];
-    fn get_qualifiers(&self) -> &[Qualifier<A>];
-    fn get_mut_qualifiers(&mut self) -> &mut [Qualifier<A>];
-    fn get_template(&self) -> &[Template<A>];
-    fn get_mut_template(&mut self) -> &mut [Template<A>];
+    fn get_slice_entitys(&self) -> &[Entity<A>];
+    fn get_mut_slice_entitys(&mut self) -> &mut [Entity<A>];
+    fn get_slice_qualifiers(&self) -> &[Qualifier<A>];
+    fn get_mut_slice_qualifiers(&mut self) -> &mut [Qualifier<A>];
+    fn get_slice_template(&self) -> &[Template<A>];
+    fn get_mut_slice_template(&mut self) -> &mut [Template<A>];
+
+    fn get_vec_entitys(&self) -> &Vec<Entity<A>>;
+    fn get_mut_vec_entitys(&mut self) -> &mut Vec<Entity<A>>;
+    fn get_vec_qualifiers(&self) -> &Vec<Qualifier<A>>;
+    fn get_mut_vec_qualifiers(&mut self) -> &mut Vec<Qualifier<A>>;
+    fn get_vec_template(&self) -> &Vec<Template<A>>;
+    fn get_mut_vec_template(&mut self) -> &mut Vec<Template<A>>;
 }
 
 /// Трейт, что реализован поумолчанию, и предоставляет интерфейс команд для работы с данными.

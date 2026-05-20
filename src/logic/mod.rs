6a00e4b41e90251815469bd14c570e8f0cec91d4
0xDimay <HexDimay@yandex.ru>
2026-05-20 21:03:27 +0300

    cargo fmt

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -1,2 +1,2 @@
-pub mod traits;
 pub mod state;
+pub mod traits;
--- src/logic/state.rs
+++ src/logic/state.rs
@@ -1,7 +1,7 @@
 /// Определяет центральное состояние системы, осуществляет подключение к базе данных и прочее.
 pub struct State {
     pub(crate) name_db: String,
-    pub(crate) database: Option<sqlite::Connection>
+    pub(crate) database: Option<sqlite::Connection>,
 }
 
 impl State {

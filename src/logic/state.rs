81cad032fb2acc0ff9a733cc286126a66d8006f3
0xDimay <HexDimay@yandex.ru>
2026-05-20 20:47:28 +0300

    Create `State`.

---
+++ src/logic/state.rs
@@ -0,0 +1,14 @@
+/// Определяет центральное состояние системы, осуществляет подключение к базе данных и прочее.
+pub struct State {
+    pub(crate) name_db: String,
+    pub(crate) database: Option<sqlite::Connection>
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            name_db: String::new(),
+            database: None,
+        }
+    }
+}
--- src/logic/traits.rs
+++ src/logic/traits.rs
@@ -1,13 +1,16 @@
-use crate::components::{
-    argument::Arg,
-    argument::TypeValue,
+use anyhow::{Ok, Result};
+use crate::{components::{
+    argument::{Arg, TypeValue},
     entity::{Entity, Value},
     qualifier::Qualifier,
     template::Template,
-};
+}, logic::state::State};
 
 /// `HandleData` - слушатель данных, что позволяет `Commander` осуществлять все требуемые комманды.
 pub trait HandleData<A: Arg> {
+    fn get_state(&self) -> &State;
+    fn get_mut_state(&mut self) -> &mut State;
+
     fn get_slice_entitys(&self) -> &[Entity<A>];
     fn get_mut_slice_entitys(&mut self) -> &mut [Entity<A>];
     fn get_slice_qualifiers(&self) -> &[Qualifier<A>];
@@ -32,4 +35,21 @@ pub trait HandleData<A: Arg> {
 /// - [ ] Формирование истории действий (транзакций)
 /// - [ ] Сбор статистики (хз, может перекинуть это на клиент)
 /// - [ ] Редактирование определённых частей данных
-pub trait Commander<A: Arg>: HandleData<A> {}
+pub trait Commander<A: Arg>: HandleData<A> {
+    /// Подключение к базе данных или же её прямое создание.
+    fn connect_database(&mut self, name_db: &str) -> Result<()> {
+        let state = self.get_mut_state();
+        state.name_db = name_db.to_owned();
+        state.database = Some(sqlite::open(name_db)?);
+
+        log::info!("Successful connection to the database.");
+
+        Ok(())
+    }
+
+    
+
+    fn get_name_database(&self) -> &str {
+        &self.get_state().name_db
+    }
+}
